c26359d13f2d67ec5625b99264f1e41215f7b466
Administrator <administrator@cpr-nav-01.cpr-nav-01>
2012-09-19 11:53:03 -0400

    Wifi monitor script now using ifconfig instead.

--- kingfisher_node/nodes/wifi_monitor.py
+++ kingfisher_node/nodes/wifi_monitor.py
@@ -5,6 +5,7 @@ import roslib; roslib.load_manifest('kingfisher_bringup')
 import rospy
 from std_msgs.msg import Bool
 from subprocess import check_output
+#from subprocess import Popen, PIPE
 
 rospy.init_node('wifi_monitor')
 
@@ -12,10 +13,15 @@ r = rospy.Rate(1)
 pub = rospy.Publisher('has_wifi', Bool)
 
 while not rospy.is_shutdown():
-  r.sleep()
   try:
-    print check_output(['wicd-cli', '--wireless', '-d'])
-    pub.publish(True)
-    continue
+    wifi_str = check_output(['ifconfig', 'wlan0']);
+    if "inet addr" in wifi_str:
+      pub.publish(True)
+    else:
+      pub.publish(False)
+    r.sleep()
+    continue 
   except:
-    pub.publish(False)
+    print "foo"
+    # Try again.
+    raise #pass
