bfcb3c695b4e0b3fb61bf75bcef15c47caa51930
Mike Purvis <mpurvis@clearpath>
2012-09-27 09:32:31 -0400

    Small fixes for kingfisher launch process.

--- kingfisher_bringup/upstart/install.bash
+++ kingfisher_bringup/upstart/install.bash
@@ -11,6 +11,7 @@ robot=kingfisher
 user=administrator
 release=$(ls /opt/ros/ | tail -n1)
 
+rm -f /etc/ros/setup.bash
 ln -s /home/${user}/ros/setup.bash /etc/ros/setup.bash
 source /etc/ros/setup.bash
 pushd `rospack find ${robot}_bringup`/upstart > /dev/null
